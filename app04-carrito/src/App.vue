6d43a91a61655bd8ea8d8cbc94f80827080d50a4
astorlucas <lucasastor@gmail.com>
2023-09-06 15:51:42 -0300

    app 4 comenzando

---
+++ app04-carrito/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6" v-for="prod in Productos" :key="prod.id">
+          <my-producto :MyProducto="prod"></my-producto>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyProducto from './components/MyProducto.vue'
+import Productos from './productos.json'
+
+export default {
+  name: 'App',
+  components: {
+    MyProducto
+  },
+  data(){
+    return{
+      Productos
+    }
+  }
+}
+</script>
+
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.css"
+</style>
---
+++ app04-carrito/src/components/MyProducto.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card my-5">
+        <div class="card-body">
+            <h5 class="text-center card-title">{{ MyProducto.titulo }}</h5>
+            <img class="text-center" :src="MyProducto.imagen" width="220" height="150" alt="TETAAAAAS">
+            <p class="text-center text-muted card-text display-3">$ {{ Number(MyProducto.precio).toFixed() }}</p>
+            <button class="btn btn-primary form-control">Agregar al carrito</button>
+        </div>
+    </div>
+</template>
+  
+<script>
+
+
+export default {
+    name: 'MyProducto',
+    props: ['MyProducto']
+}
+</script>
+  
+<style></style>
